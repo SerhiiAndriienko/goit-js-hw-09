b8fd6d6c107f7ba9fa2a7c4d79687d22e73645b2
SerhiiAndriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-04-08 16:11:48 +0000

    Deploying to gh-pages from @ SerhiiAndriienko/goit-js-hw-09@d891ac66c69ed44ef9b726015eeac920d44ebafe 🚀

---
+++ 01-color-switcher.fc8c8c4d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),e.removeAttribute("disabled","disabled"),d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){this.setAttribute("disabled","disabled"),clearInterval(d),t.removeAttribute("disabled","disabled")}));
+//# sourceMappingURL=01-color-switcher.fc8c8c4d.js.map
